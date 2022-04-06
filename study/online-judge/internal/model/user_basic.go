b12700f0963d055d0ba713194372e2d287f68484
ol2690539295 <2690539295@qq.com>
2022-04-06 23:25:59 +0800

    修改UserBasic的信息

--- study/online-judge/internal/model/user_basic.go
+++ study/online-judge/internal/model/user_basic.go
@@ -3,11 +3,13 @@ package model
 import "gorm.io/gorm"
 
 type UserBasic struct {
-	Identity string `gorm:"column:identity;type:varchar(36);" json:"identity"`
-	Name     string `gorm:"column:name;type:varchar(100);" json:"name"`
-	Password string `gorm:"column:password;type:varchar(32);" json:"password"`
-	Phone    string `gorm:"column:phone;type:varchar(20);" json:"phone"`
-	Email    string `gorm:"column:email;type:varchar(100);" json:"email"`
+	Identity         string `gorm:"column:identity;type:varchar(36);" json:"identity"`
+	Name             string `gorm:"column:name;type:varchar(100);" json:"name"`
+	Password         string `gorm:"column:password;type:varchar(32);" json:"password"`
+	Phone            string `gorm:"column:phone;type:varchar(20);" json:"phone"`
+	Email            string `gorm:"column:email;type:varchar(100);" json:"email"`
+	FinishProblemNum int64  `gorm:"column:finish_problem_num;type:int(11);" json:"finish_problem_num"`
+	SubmitNum        int64  `gorm:"column:submit_num;type:int(11);" json:"submit_num"`
 	gorm.Model
 }
 
