521140821a49800ade9d39a33393a7b929c0d221
ol2690539295 <2690539295@qq.com>
2022-04-06 23:35:56 +0800

    更新路由

--- study/online-judge/internal/router/router.go
+++ study/online-judge/internal/router/router.go
@@ -17,15 +17,27 @@ func Router() *gin.Engine {
 		v1.GET("/ping", service.Ping)
 	}
 	// 问题
-	r.GET("/problem", service.GetProblemList)
-	r.GET("/problem/:identity", service.GetProblem)
+	{
+		r.GET("/problem", service.GetProblemList)
+		r.GET("/problem/:identity", service.GetProblem)
+	}
+	// 排行榜
+	{
+		r.GET("/rank", service.GetRankList)
+	}
 
 	// 用户
-	r.GET("/user/:identity", service.GetUser)
-	r.POST("/v1/login", service.Login)
+	{
+		r.GET("/user/:identity", service.GetUser)
+		r.POST("/v1/login", service.Login)
+		r.POST("/v1/send", service.SendCode)
+		r.POST("/v1/register", service.Register)
+	}
 
 	// 提交记录
-	r.GET("/submit", service.GetSubmitList)
+	{
+		r.GET("/submit", service.GetSubmitList)
+	}
 
 	return r
 }
