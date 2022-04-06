5d8b46b2b7177b3db85672520424908e24f177ba
ol2690539295 <2690539295@qq.com>
2022-04-06 23:38:06 +0800

    添加代码提交测试

---
+++ study/online-judge/code/runner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+)
+
+func main() {
+	// go run code-user/main.go
+	cmd := exec.Command("go", "run", "code-user/main.go")
+	var out, stdErr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stdErr
+	pipe, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	io.WriteString(pipe, "23 11\n")
+	// 根据输入的测试案例，拿到输入结果和标准输出结果对比
+	if err := cmd.Run(); err != nil {
+		log.Fatalln(err, stdErr.String())
+		return
+	}
+	fmt.Println(out.String() == "34\n")
+}
