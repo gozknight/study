d152d2c18575d3863967709e8d3aa3a3c6bc4334
ol2690539295 <2690539295@qq.com>
2022-04-06 23:34:48 +0800

    添加Redis配置

--- study/online-judge/internal/model/base.go
+++ study/online-judge/internal/model/base.go
@@ -1,15 +1,25 @@
 package model
 
 import (
+	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 )
 
-var ORM = Init()
+var ORM = InitMySQL()
+var RDB = InitRedis()
 
-func Init() *gorm.DB {
+func InitRedis() *redis.Client {
+	var rdb = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	return rdb
+}
+func InitMySQL() *gorm.DB {
 	url := "root:root@tcp(127.0.0.1:3306)/online-judge?charset=utf8&parseTime=true&loc=Local"
 	orm, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
