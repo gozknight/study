4b7079b1a15fafbd1b646aad1f56d668b29b0555
gozknight <2690539295@qq.com>
2022-04-07 17:48:05 +0800

    添加增加问题方法

--- study/online-judge/internal/service/problem.go
+++ study/online-judge/internal/service/problem.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"gozknight.com/online-judge/internal/model"
@@ -90,4 +91,96 @@ func GetProblem(c *gin.Context) {
 		"code": -1,
 		"data": data,
 	})
+}
+
+// AddProblem
+// @Tags 私有方法
+// @Summary 添加问题
+// @Param authorization header string true "authorization"
+// @Param title formData string true "tile"
+// @Param content formData string true "content"
+// @Param max_runtime formData string true "max_runtime"
+// @Param max_memory formData string true "max_memory"
+// @Param category_ids formData array true "category_ids"
+// @Param test_cases formData array true "test_cases"
+// @Success 200 {string} json "{"code":"200","msg":""}"
+// @Router /admin/problem/add [post]
+func AddProblem(c *gin.Context) {
+	title := c.PostForm("title")
+	content := c.PostForm("content")
+	maxRuntime := c.PostForm("max_runtime")
+	maxMemory := c.PostForm("max_memory")
+	categoryIds := c.PostFormArray("category_ids")
+	testCases := c.PostFormArray("test_cases")
+	if title == "" || content == "" || maxRuntime == "" || maxMemory == "" || len(categoryIds) == 0 || len(testCases) == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "参数不能为空",
+		})
+		return
+	}
+	identity := util.GetUuid()
+	problem := &model.ProblemBasic{
+		Identity:   identity,
+		Title:      title,
+		Content:    content,
+		MaxRuntime: maxRuntime,
+		MaxMemory:  maxMemory,
+	}
+	categoryBasics := make([]*model.ProblemCategory, 0)
+	for _, id := range categoryIds {
+		cid, _ := strconv.Atoi(id)
+		categoryBasics = append(categoryBasics, &model.ProblemCategory{
+			ProblemId:  problem.ID,
+			CategoryId: uint(cid),
+		})
+	}
+	problem.ProblemCategories = categoryBasics
+
+	testCaseBasics := make([]*model.TestCase, 0)
+	for _, test := range testCases {
+		testCaseMap := make(map[string]string)
+		err := json.Unmarshal([]byte(test), &testCaseMap)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code": -1,
+				"msg":  "测试用例错误",
+			})
+			return
+		}
+		if _, ok := testCaseMap["input"]; !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"code": -1,
+				"msg":  "测试用例错误",
+			})
+			return
+		}
+		if _, ok := testCaseMap["output"]; !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"code": -1,
+				"msg":  "测试用例错误",
+			})
+			return
+		}
+		testCaseBasics = append(testCaseBasics, &model.TestCase{
+			Identity:        util.GetUuid(),
+			ProblemIdentity: identity,
+			Input:           testCaseMap["input"],
+			Output:          testCaseMap["output"],
+		})
+	}
+	problem.TestCase = testCaseBasics
+	err := model.ORM.Create(problem).Error
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "创建问题失败",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "问题创建成功",
+	})
+
 }
