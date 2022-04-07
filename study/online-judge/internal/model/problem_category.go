ca0c51be8883d2c3be687aecd839f3cb13946a56
gozknight <2690539295@qq.com>
2022-04-07 17:49:14 +0800

    修改结构体信息

--- study/online-judge/internal/model/problem_category.go
+++ study/online-judge/internal/model/problem_category.go
@@ -3,8 +3,8 @@ package model
 import "gorm.io/gorm"
 
 type ProblemCategory struct {
-	ProblemId     int            `gorm:"column:problem_id;type:int(11)" json:"problem_id"`
-	CategoryId    int            `gorm:"column:category_id;type:int(11)" json:"category_id"`
+	ProblemId     uint           `gorm:"column:problem_id;type:int(11)" json:"problem_id"`
+	CategoryId    uint           `gorm:"column:category_id;type:int(11)" json:"category_id"`
 	CategoryBasic *CategoryBasic `gorm:"foreignKey:id;references:category_id"`
 	gorm.Model
 }
