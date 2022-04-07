5e3dccc7495492de0bcb2dc3e5cf6e0d82f072b0
gozknight <2690539295@qq.com>
2022-04-07 17:47:14 +0800

    添加权限认证

---
+++ study/online-judge/internal/middleware/auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"gozknight.com/online-judge/internal/util"
+	"net/http"
+)
+
+// AuthAdmin 管理员验证
+func AuthAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("authorization")
+		userClaim, err := util.AnalyzeToken(auth)
+		if err != nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized err",
+			})
+			return
+		}
+		if userClaim.IsAdmin != 1 {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized not admin",
+			})
+			return
+		}
+		c.Next()
+	}
+}
