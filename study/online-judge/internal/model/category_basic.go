bdab4df3941fcceade9f5406ac971fbc6fe54b28
gozknight <2690539295@qq.com>
2022-04-10 21:38:29 +0800

    修改parentId类型

--- study/online-judge/internal/model/category_basic.go
+++ study/online-judge/internal/model/category_basic.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type CategoryBasic struct {
 	Identity string `gorm:"column:identity;type:varchar(36);" json:"identity"`
 	Name     string `gorm:"column:name;type:varchar(100);" json:"name"`
-	ParentId string `gorm:"column:parent_id;type:int(11);" json:"parent_id"`
+	ParentId int    `gorm:"column:parent_id;type:int(11);" json:"parent_id"`
 	gorm.Model
 }
 
